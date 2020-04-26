466f344dcab2c4e402a5dcf4cb8b3ff05c1a18f7
akclifto <akclifto@asu.edu>
2020-04-25 17:33:44 -0700

    adding new class files

---
+++ src/ser321/assign6/akclifto/server/LibraryServerSkeleton.java
@@ -0,0 +1,35 @@
+package ser321.assign6.akclifto.server;
+
+
+/**
+ * Copyright 2020 Adam Clifton, akclifto@asu.edu
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * <p>
+ * This module acts as the View layer for your application.
+ * The 'MediaLibraryGui' class actually builds the Gui with all
+ * the components - buttons, text fields, text areas, panels etc.
+ * This class should be used to write the logic to add functionality
+ * to the Gui components.
+ * You are free add more files and further modularize this class's
+ * functionality.
+ * <p>
+ * Purpose: LibraryServerSkeleton is the server-side TCP/IP skeleton used to communicate with the
+ * client-side TCP/IP stub to implement JsonRPC requests and responses.
+ *
+ * @author Adam Clifton (akclifto@asu.edu)
+ * Software Engineering, ASU
+ * @version April 2020
+ */
+public class LibraryServerSkeleton {
+}
